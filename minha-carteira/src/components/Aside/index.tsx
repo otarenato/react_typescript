8c5c2cbc497877a62a78b3ee1a264b74c2d39552
Renato Ota <renato.ota@fgv.br>
2023-04-25 16:18:40 -0300

    Criação do menu lateral

--- minha-carteira/src/components/Aside/index.tsx
+++ minha-carteira/src/components/Aside/index.tsx
@@ -1,16 +1,36 @@
 import React from "react";
 import logoImg from '../../assets/logo.svg';
-import { Container, Header, LogImg, MenuContainer, MenuItemLink } from "./styles";
+import { Container, Header, LogImg, MenuContainer, MenuItemLink, Title } from "./styles";
+import {
+    MdDashboard,
+    MdArrowDownward,
+    MdArrowUpward,
+    MdExitToApp
+} from 'react-icons/md';
 
 const Aside: React.FC = () => {
     return (
         <Container>
             <Header>
                 <LogImg src={logoImg} alt="Logo Minha Carteira" />
+                <Title>Minha Carteira</Title>
             </Header>
             <MenuContainer>
                 <MenuItemLink href="#">
+                    <MdDashboard />
                     Dashboard
+                </MenuItemLink>
+                <MenuItemLink href="#">
+                    <MdArrowUpward />
+                    Entradas
+                </MenuItemLink>
+                <MenuItemLink href="#">
+                    <MdArrowDownward />
+                    Saídas
+                </MenuItemLink>
+                <MenuItemLink href="#">
+                    <MdExitToApp />
+                    Sair
                 </MenuItemLink>
             </MenuContainer>
         </Container>
