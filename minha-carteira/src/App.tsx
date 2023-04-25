6837aa6deb1681feaed5f03fef3ed91556f14b93
Renato Ota <renato.ota@fgv.br>
2023-04-24 22:44:51 -0300

    Criação de componentes e temas

--- minha-carteira/src/App.tsx
+++ minha-carteira/src/App.tsx
@@ -1,14 +1,16 @@
 import React from "react";
 import GlobalStyles from "./styles/GlobalStyles";
+import { ThemeProvider } from "styled-components";
 
-import Dashboard from "./pages/Dashboard/Index";
+import Layout from "./components/Layout";
+import dark from "./styles/themes/dark";
 
 const App: React.FC = () => {
     return (
-        <>
+        <ThemeProvider theme={dark}>
             <GlobalStyles />
-            <Dashboard />
-        </>
+            <Layout />
+        </ThemeProvider>
     );
 }
 
